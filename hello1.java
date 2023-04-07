446ace98585c83ccbcb937654ed680a8e07fcda8
Velkaandhans <61485415+Velkaandhans@users.noreply.github.com>
2023-04-07 23:37:32 +0530

    Update hello1.java

--- hello1.java
+++ hello1.java
@@ -2,7 +2,7 @@ package hello;
 public class hello1 {
 	public static void main(String[]args){
 		System.out.println("hello World...");
-		System.out.println("hello World2...");
+		System.out.println("hello World23...");
 	}
 
 }
